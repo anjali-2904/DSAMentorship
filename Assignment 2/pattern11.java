7599258142e3018f5dafff2750aea2ddc6fb527d
Anjali Singhal <92749087+anjali-2904@users.noreply.github.com>
2023-12-30 19:56:50 +0530

    Add files via upload

---
+++ Assignment 2/pattern11.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class pattern11 {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int n=sc.nextInt();
+        int k=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(k+" ");
+                k++;
+            }
+            System.out.println();
+
+        }
+        sc.close();
+    }
+}
