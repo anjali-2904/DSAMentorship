af527227481796ca09904d75943f176b54736c8f
Anjali Singhal <92749087+anjali-2904@users.noreply.github.com>
2024-02-16 19:48:47 +0530

    Update PeakElement.java

--- Assignment 3/PeakElement.java
+++ Assignment 3/PeakElement.java
@@ -1,1 +1,28 @@
-
+class Solution
+{
+	// Function to find the peak element
+	// arr[]: input array
+	// n: size of array a[]
+	public int peakElement(int[] arr,int n)
+    {
+       //add code here.
+       if(n==1)
+       return 0;
+       if(arr[0]>=arr[1])
+       return 0;
+       if(arr[n-1]>=arr[n-2])
+       return n-1;
+       int start=0;
+       int end=n-2;
+       while(start<=end){
+           int mid=start+(end-start)/2;
+           if(arr[mid]>=arr[mid-1] && arr[mid]>=arr[mid+1])
+           return mid;
+           else if(arr[mid]<arr[mid+1])
+           start=mid+1;
+           else
+           end=mid-1;
+       }
+       return -1;
+    }
+}
